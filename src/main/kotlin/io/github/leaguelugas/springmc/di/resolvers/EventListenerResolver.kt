e3294477b2f7787342f6431dc34171bcc741b3e5
Kaisa <kaisa@supergene.co>
2024-08-29 18:26:06 +0900

    Update event listener to more easy

--- src/main/kotlin/io/github/leaguelugas/springmc/di/resolvers/EventListenerResolver.kt
+++ src/main/kotlin/io/github/leaguelugas/springmc/di/resolvers/EventListenerResolver.kt
@@ -4,7 +4,6 @@ import io.github.leaguelugas.springmc.SpringMC
 import io.github.leaguelugas.springmc.di.BeanResolver
 import io.github.leaguelugas.springmc.di.annotations.EventListener
 import io.github.leaguelugas.springmc.event.SpringListener
-import org.bukkit.event.Event
 
 class EventListenerResolver(
     private val plugin: SpringMC,
@@ -14,34 +13,14 @@ class EventListenerResolver(
         annotation: EventListener,
     ): Any {
         val listenerClass = "${instance.javaClass.`package`}.${instance.javaClass.simpleName}"
-        if (instance !is SpringListener<*>) {
+        if (instance !is SpringListener) {
             throw IllegalArgumentException(
                 "Event-Listener ($listenerClass) must implement SpringListener interface",
             )
         }
 
-        val eventClass: Class<out Event> =
-            instance.javaClass.declaredMethods
-                .find { it.name == "onEvent" && it.parameters.isNotEmpty() }
-                ?.parameters
-                ?.let {
-                    it.forEach { p ->
-                        runCatching {
-                            return@let p.type.asSubclass(Event::class.java)
-                        }
-                    }
-                    return@let null
-                }
-                ?: throw IllegalArgumentException(
-                    "Event-Listener ($listenerClass) must have a method named 'onEvent' with at least one parameter extending Event",
-                )
-
         plugin.afterLoadingJob.add {
-            plugin.server.pluginManager.registerEvent(eventClass, instance, annotation.priority, { listener, event ->
-                if (listener is SpringListener<Event>) {
-                    listener.onEvent(event)
-                }
-            }, plugin)
+            plugin.server.pluginManager.registerEvents(instance, plugin)
         }
 
         return instance
--- src/main/kotlin/io/github/leaguelugas/springmc/event/SpringListener.kt
+++ src/main/kotlin/io/github/leaguelugas/springmc/event/SpringListener.kt
@@ -1,8 +1,5 @@
 package io.github.leaguelugas.springmc.event
 
-import org.bukkit.event.Event
 import org.bukkit.event.Listener
 
-interface SpringListener<out T : Event> : Listener {
-    fun onEvent(event: @UnsafeVariance T)
-}
+interface SpringListener : Listener
